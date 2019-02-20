892d047d1fec1f2e6c31ec7cbbe630aeecffc7c0
Vorobiov Vlad <vorobyov.vladislav@gmail.com>
2019-02-20 12:12:28 +0200

    Updates

---
+++ precache-manifest.953d853256b9484a54805d735fac4a8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23b1f79e7f26834c9527",
+    "url": "/swapi/static/js/runtime~main.23b1f79e.js"
+  },
+  {
+    "revision": "6b2e2c3b5f09791030d8",
+    "url": "/swapi/static/js/main.6b2e2c3b.chunk.js"
+  },
+  {
+    "revision": "6beebef9e5039e0dbb4f",
+    "url": "/swapi/static/js/2.6beebef9.chunk.js"
+  },
+  {
+    "revision": "6b2e2c3b5f09791030d8",
+    "url": "/swapi/static/css/main.15245a7d.chunk.css"
+  },
+  {
+    "revision": "33c2b01296ccd91aecfa2208eb3079e4",
+    "url": "/swapi/index.html"
+  }
+];
